9a5ad5ae0ff83c1e408074449181f0c594fb8371
Ale R. Buteler <alebuteler@gmail.com>
2020-07-27 21:30:21 -0300

    Config.

--- tests-app/config.js
+++ tests-app/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'Gibberish, jibberish, jibber-jabber and gobbledygook',
   // DB_CONNECTION: process.env.DB_CONNECTION || 'mongodb:27017/zoom', // docker-compose container
-  DB_CONNECTION: process.env.DB_CONNECTION || 'mongodb+srv://abuteler:OOiYYk0RrX880xPX@cluster0.2p1xm.gcp.mongodb.net/actus?retryWrites=true&w=majority',
+  DB_CONNECTION: process.env.DB_CONNECTION || 'mongodb+srv://abuteler:password@cluster0.2p1xm.gcp.mongodb.net/actus?retryWrites=true&w=majority',
   DB_NAME: process.env.DB_NAME || 'actus',
   DB_COLLECTION: process.env.DB_COLLECTION || 'big5devtesting',
   URL: process.env.URL || 'http://localhost:3000'
